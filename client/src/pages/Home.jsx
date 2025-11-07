2cf17381b2e62d2a3d0b63222e7e98be94793b49
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2025-11-08 04:50:00 +0530

    New UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import banner from '../assets/banner.jpg';
+import bannerr from '../assets/bannerr.png';
 import bannerMobile from '../assets/banner-mobile.jpg';
+import bannerMobilee from '../assets/bannerMobile.png';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { valideURLConvert } from '../utils/valideURLConvert';
@@ -26,20 +27,28 @@ const Home = () => {
   return (
     <section className='bg-white'>
       {/* Banner */}
-      <div className='container mx-auto'>
-        <div className={`w-full h-full min-h-48 bg-blue-100 rounded ${!banner && 'animate-pulse my-2'}`}>
-          <img
-            src={banner}
-            className='w-full h-full hidden lg:block'
-            alt='Desktop banner'
-          />
-          <img
-            src={bannerMobile}
-            className='w-full h-full lg:hidden'
-            alt='Mobile banner'
-          />
-        </div>
-      </div>
+<div className="container mx-auto">
+  <div
+    className={`w-full bg-blue-100 rounded overflow-hidden ${
+      !bannerr && "animate-pulse my-2"
+    }`}
+  >
+    {/* Desktop / large screens */}
+    <img
+      src={bannerr}
+      alt="Desktop banner"
+      className="hidden lg:block w-full h-auto max-h-[350px] object-cover object-center"
+    />
+
+    {/* Mobile / small screens */}
+    <img
+      src={bannerMobilee || bannerr}
+      alt="Mobile banner"
+      className="block lg:hidden w-full h-[180px] sm:h-[220px] md:h-[260px] object-cover object-center"
+    />
+  </div>
+</div>
+
 
       {/* Category Grid */}
       <div className='container mx-auto px-4 my-2 grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10 gap-2'>
